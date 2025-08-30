4375bb395a38e311e6287379997f0a71ea4c7635
MariaAcosta1 <L25550705@chihuahua2.tecnm.mx>
2025-08-29 22:08:19 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/EVA1_5_VARIABLES.java
@@ -0,0 +1,22 @@
+
+package eva1_5_variables;
+
+public class EVA1_5_VARIABLES {
+
+    public static void main(String[] args) {
+        //DECLARACIÓN DE VARIABLES
+        //crear las variables para que se puede usar
+        //TIPO DE DATO IDENTIFICADOR
+        String nombre;
+        byte edad;
+        //INICIALIZACIÓN
+        //USAMOS EL OPERADOR = --> OPERADOR DE ASIGNACIÓN
+        //PON "Mariana Acosta" dentro de la variable nombre
+        nombre = "Mariana Acosta";
+        edad = 17;
+        //USO DE VARIABLES
+        System.out.println(nombre);
+        System.out.println(edad);
+    }
+    
+}
