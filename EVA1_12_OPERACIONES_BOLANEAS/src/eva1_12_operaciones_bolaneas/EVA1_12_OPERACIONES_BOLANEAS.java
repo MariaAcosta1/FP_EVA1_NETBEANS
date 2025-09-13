7efe881dc75e2e2cc20ff5541c299c386cdb1164
MariaAcosta1 <L25550705@chihuahua2.tecnm.mx>
2025-09-12 22:20:52 -0600

    Add files via upload

---
+++ EVA1_12_OPERACIONES_BOLANEAS/src/eva1_12_operaciones_bolaneas/EVA1_12_OPERACIONES_BOLANEAS.java
@@ -0,0 +1,31 @@
+
+package eva1_12_operaciones_bolaneas;
+
+public class EVA1_12_OPERACIONES_BOLANEAS {
+
+    public static void main(String[] args) {
+        int radio = 5;//declaro e inicializo
+        boolean resu;//true-false
+        //USO DE OPERADORES BOOLEANOS:
+        
+        resu = radio < 0;
+        System.out.print("Radio (5) < 0 es: ");
+        System.out.println(resu);
+        System.out.println(" ");
+        
+        resu = radio > 0;//radio (5) > 0
+        System.out.print("Radio (5) > 0 es: ");
+        System.out.println(resu);
+        System.out.println(" ");
+        
+        resu = radio == 0;//radio (5) == 0
+        System.out.print("Radio (5) = 0 es: ");
+        System.out.println(resu);
+        System.out.println(" ");
+        
+        resu = radio !=0;//radio (5) !=0
+        System.out.print("Radio (5) !=0 es: ");
+        System.out.println(resu);
+    }
+    
+}
