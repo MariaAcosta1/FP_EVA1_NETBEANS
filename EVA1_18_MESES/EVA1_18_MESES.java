fc79c276821015600f53de63975195fef7a7978c
MariaAcosta1 <L25550705@chihuahua2.tecnm.mx>
2025-09-13 02:15:50 -0600

    Add files via upload

---
+++ EVA1_18_MESES/EVA1_18_MESES.java
@@ -0,0 +1,44 @@
+
+package eva1_18_meses;
+
+import java.util.Scanner;
+
+public class EVA1_18_MESES {
+
+    public static void main(String[] args) {
+        int mes;
+        Scanner captu = new Scanner(System.in);
+        System.out.println("Número del mes: ");
+        mes = captu.nextInt();
+        System.out.println(" ");
+        //else - if --> if anidado
+        if(mes == 1){
+            System.out.println("Enero.");
+        }else if(mes == 2){
+            System.out.println("Febrero.");
+        }else if(mes == 3){
+            System.out.println("Marzo.");
+        }else if(mes == 4){
+            System.out.println("Abril.");
+        }else if(mes == 5){
+            System.out.println("Mayo.");
+        }else if(mes == 6){
+            System.out.println("Julio.");
+        }else if(mes == 7){
+            System.out.println("Junio.");
+        }else if(mes == 8){
+            System.out.println("Agosto.");
+        }else if(mes == 9){
+            System.out.println("Septiembre.");
+        }else if(mes == 10){
+            System.out.println("Octubre.");
+        }else if(mes == 11){
+            System.out.println("Noviembre.");
+        }else if(mes == 12){
+            System.out.println("Diciembre.");    
+        }else{
+            System.out.println("Numero de mes no es valido!");
+        }
+    }
+    
+}
